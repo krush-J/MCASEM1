81956e2c31533317688a794d7c4978c401c64451
krush-J <74696489+krush-J@users.noreply.github.com>
2022-12-22 18:19:39 +0530

    Delete pr2_module directory

--- pr2_module/Circle.js
+++
@@ -1,4 +0,0 @@
-exports.area = (r) =>    
-         console.log(`Area of circle with ${r} is ${(3.14*r*r).toFixed(2)}`);
-exports.perimeter = (r) =>   
-         console.log(`Area of circle with ${r} is ${(2*3.14*r).toFixed(2)}`);
--- pr2_module/CircleUtility.js
+++
@@ -1,7 +0,0 @@
-/**
- * Create a circle module with functions to find the area and perimeter
- *  of a circle and use it.
- */
-circle = require('./Circle');
-circle.area(12);
-circle.perimeter(3);
